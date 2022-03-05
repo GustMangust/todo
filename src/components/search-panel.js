25964f86c0cf677aeec94d11606785e44c70b6ce
GustMangust <gust.mangust@gmail.com>
2022-03-05 14:07:11 +0300

    Added more updates

--- src/components/search-panel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {fontSize: '20px'};
-    return <input placeholder={searchText} style={searchStyle}></input>;
-  };
-
-  export default SearchPanel;
